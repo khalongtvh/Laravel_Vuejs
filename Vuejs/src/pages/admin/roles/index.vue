226897f1941c640d2753c129ee947b44080d28f4
khalongtvh <khalongtvh@gmail.com>
2023-01-16 20:41:42 +0700

    s

--- Vuejs/src/pages/admin/roles/index.vue
+++ Vuejs/src/pages/admin/roles/index.vue
@@ -1,3 +1,12 @@
 <template>
     roles
 </template>
+<script>
+import { useMenu } from '../../../store/use-Menu.js'
+
+export default {
+    setup() {
+        useMenu().onSelectedKeys(["admin-roles"])
+    }
+}
+</script>
--- Vuejs/src/pages/admin/settings/index.vue
+++ Vuejs/src/pages/admin/settings/index.vue
@@ -3,3 +3,12 @@
         settings
     </p>
 </template>
+<script>
+import { useMenu } from '../../../store/use-Menu.js'
+
+export default {
+    setup() {
+        useMenu().onSelectedKeys(["admin-settings"])
+    }
+}
+</script>
